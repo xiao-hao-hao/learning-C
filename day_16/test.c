fefd5006830d032401be31d85a5126891c32aba8
haohao <1783692558@qq.com>
2019-08-26 23:30:55 +0800

    提交代码

--- day_16/test.c
+++ day_16/test.c
@@ -176,13 +176,87 @@
 //	return 0;
 //}
 
+//#include <stdio.h>
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = (int(*)[4])a;
+//	printf("%p, %d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);//地址减地址得到相差的（该指针所指元素类型）元素个数
+//	printf("%p\n%x\n", 10, 10);
+//	return 0;
+//}
+
+//用冒泡排序法实现qsort函数
 #include <stdio.h>
+
+struct Stu
+{
+	char name[20];
+	short age;
+}
+
+int cmp_int(const void *e1, const void *e2)
+{
+	return *(int *)e1 - *(int *)e2;
+}
+
+void print_arr(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void Swap(char *buf1, char *buf2, int width)
+{
+	int i = 0;
+	char tmp = 0;
+	for (i = 0; i < width; i++)
+	{
+		tmp = *(buf2 + i);
+		*(buf2 + i) = *(buf1 + i);
+		*(buf1 + i) = tmp;
+	}
+}
+
+void bubble_sort(void *base, int sz, int width, int (*cmp)(const void *e1, const void *e2))
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (j = 0; j < sz - i - 1; j++)
+		{
+			if (cmp((char *)base+width*j, (char *)base+width*(j+1)) > 0)
+			{
+				Swap((char *)base + width*j, (char *)base+width*(j+1), width);
+			}
+		}
+	}
+}
+
+void Test1()
+{
+	int arr[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+	print_arr(arr, sz);
+}
+
+void Test2()
+{
+	struct Stu arr[] = {{"zhangsan", 20}, {"lisi", 15}, {"wangwu", 25}};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz, sizeof(arr[0]), cmp_stu_by_name);
+	bubble_sort(arr, sz, sizeof(arr[0]), cmp_stu_by_age);
+}
+
 int main()
 {
-	int a[5][5];
-	int(*p)[4];
-	p = (int(*)[4])a;
-	printf("%p, %d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);//地址减地址得到相差的（该指针所指元素类型）元素个数
-	printf("%p\n%x\n", 10, 10);
+	Test1();
 	return 0;
 }
