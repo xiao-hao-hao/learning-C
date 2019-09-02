81efe99f4e7037a98b1a6028429aeb798daf0db4
haohao <1783692558@qq.com>
2019-09-02 22:24:56 +0800

    提交代码

--- day_20/test.cpp
+++ day_20/test.cpp
@@ -91,20 +91,50 @@
 //	print2(&s); //传地址
 //	return 0;
 //}
+//#include <stdio.h>
+//union Un 
+//{ 
+//	 int i; 
+//	 char c; 
+//}; 
+//int main()
+//{
+//	union Un un; 
+//	// 下面输出的结果是一样的吗？
+//	printf("%d\n", &(un.i)); 
+//	printf("%d\n", &(un.c)); 
+//	//下面输出的结果是什么？
+//	un.i = 0x11223344; 
+//	un.c = 0x55; 
+//	printf("%x\n", un.i);
+//}
+
 #include <stdio.h>
-union Un 
-{ 
-	 int i; 
-	 char c; 
-}; 
 int main()
 {
-	union Un un; 
-	// 下面输出的结果是一样的吗？
-	printf("%d\n", &(un.i)); 
-	printf("%d\n", &(un.c)); 
-	//下面输出的结果是什么？
-	un.i = 0x11223344; 
-	un.c = 0x55; 
-	printf("%x\n", un.i);
+	int a = 10000;
+	//char *a = "hello123";
+	FILE *pf = fopen("test.txt", "w");
+	fwrite(&a, 4, 1, pf);//二进制的形式写到文件中（每次写4个字节，一共写一次）
+	fclose(pf);
+	pf = NULL;
+	return 0;
 }
+
+//#include <stdio.h>
+//struct S
+//{
+//	char a:3;
+//	char b:4;
+//	char c:5;
+//	char d:4;
+//};
+//int main()
+//{
+//	struct S s = {0};
+//	s.a = 10;
+//	s.b = 12;
+//	s.c = 3;
+//	s.d = 4;
+//	return 0;
+//}
