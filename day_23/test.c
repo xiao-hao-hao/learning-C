62fe0029b6c3d235336c306408b12e9806c724b7
haohao <1783692558@qq.com>
2019-09-08 21:19:35 +0800

    提交代码

--- day_23/test.c
+++ day_23/test.c
@@ -182,3 +182,27 @@
 //	return 0;
 //}
 
+#include <stdlib.h>
+typedef struct st_type
+{
+	int i;
+	int *p_a;
+}type_a;
+
+int main()
+{
+	int i = 0;
+	type_a *p = (type_a*)malloc(sizeof(type_a));
+	p->i = 100;
+	p->p_a = (int*)malloc(p->i*sizeof(int));
+	//业务处理
+	for (i = 0; i < 100; ++i)
+	{
+		p->p_a[i] = i;
+	}
+	free(p->p_a);
+	p->p_a = NULL;
+	free(p);
+	p = NULL;
+	return 0;
+}
