d5961f487ea781c023519e6ac719576fa07df14e
haohao <1783692558@qq.com>
2019-09-20 17:19:20 +0800

    提交代码

--- day_23/test.c
+++ day_23/test.c
@@ -162,3 +162,23 @@
 //}
 
 
+//#include <stdlib.h>
+//typedef struct st_type
+//{
+//	int i;
+//	int a[0];//柔性数组成员
+//}type_a;
+//
+//int main()
+//{
+//	int i = 0;
+//	type_a *p = (type_a*)malloc(sizeof(type_a)+100*sizeof(int));
+//	//业务处理
+//	p->i = 100;
+//	for (i = 0; i < 100; ++i)
+//	{
+//		p->a[i] = i;
+//	}
+//	free(p);
+//	return 0;
+//}
