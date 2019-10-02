29482c5b51766a6bad46503a941d53da7cf8e6ae
haohao <1783692558@qq.com>
2019-10-02 19:19:45 +0800

    提交代码

--- DataStruct/seqlist.c
+++ DataStruct/seqlist.c
@@ -7,7 +7,9 @@ int main()
 {
 	int select = 1;
 	DataType data = 0;
+	DataType dest = 0;
 	int pos = 0;
+	int ret = 0;
     SeqList mylist;
     SeqListInit(&mylist, SEQLIST_DEFAULT_SIZE);
     while(select)
@@ -18,6 +20,10 @@ int main()
         printf("* [4]pop_back    [5]pop_front  *\n");
         printf("* [6]insert_pos  [7]insert_val *\n");
         printf("* [8]delete_pos  [9]delete_val *\n");
+        printf("* [10]find_pos   [11]find_val  *\n");
+        printf("* [12]sort       [13]reverse   *\n");
+        printf("* [14]length     [15]clear     *\n");
+        printf("* [16]modify_pos [17]modify_val*\n");
         printf("********************************\n");
         printf("请选择:>");
         scanf("%d", &select);
@@ -64,9 +70,57 @@ int main()
 		case 8:
 			printf("请输入要删除的位置:>");
 			scanf("%d", &pos);
-			DeleteByPos(&mylist, pos);
+			SeqListDeleteByPos(&mylist, pos);
 			break;
-        default:
+		case 9:
+			printf("请输入要删除的数据:>");
+			scanf("%d", &data);
+			SeqListDeleteByVal(&mylist, data);
+			break;
+		case 10:
+			printf("请输入要查找的位置:>");
+			scanf("%d", &pos);
+			ret = SeqListFindByPos(&mylist, pos, &data);
+			if(ret)
+				printf("位置%d处的值为:> %d\n", pos, data);
+			break;
+		case 11:
+			printf("请输入要查找的数据:>");
+			scanf("%d", &data);
+			ret = SeqListFindByVal(&mylist, data);
+			if(-1 != ret)
+				printf("%d的位置是:> %d\n", data, ret);
+			else
+				printf("要查找的数据不存在!\n");
+			break;
+		case 12:
+			SeqListSort(&mylist);
+			break;
+		case 13:
+			SeqListReverse(&mylist);
+			break;
+		case 14:
+			ret = SeqListLength(&mylist);
+			printf("顺序表的长度为:> %d\n", ret);
+			break;
+		case 15:
+			SeqListClear(&mylist);
+			break;
+		case 16:
+			printf("请输入要修改的位置:>");
+			scanf("%d", &pos);
+			printf("请输入目标值:>");
+			scanf("%d", &data);
+			SeqListModifyByPos(&mylist, pos, data);
+			break;
+		case 17:
+			printf("请输入要修改的数据:>");
+			scanf("%d", &dest);
+			printf("请输入目标值:>");
+			scanf("%d", &data);
+			SeqListModifyByVal(&mylist, dest, data);
+			break;
+		default:
             printf("输入错误，请重新输入!\n");
             break;
         }
--- DataStruct/seqlist.h
+++ DataStruct/seqlist.h
@@ -14,6 +14,8 @@ typedef struct SeqList
 
 void SeqListInit(SeqList *psl, int sz);
 int SeqListIsFull(SeqList *psl);
+void Swap(DataType *pa, DataType *pb);
+
 void SeqListShow(SeqList *psl);
 int SeqListPushFront(SeqList *psl, DataType data);
 int SeqListPushBack(SeqList *psl, DataType data);
@@ -21,11 +23,19 @@ int SeqListPopFront(SeqList *psl);
 int SeqListPopBack(SeqList *psl);
 int SeqListInsertByPos(SeqList *psl, int pos, DataType data);
 int SeqListInsertByVal(SeqList *psl, DataType data);//要求顺序表有序
-int DeleteByPos(SeqList *psl, int pos);
+int SeqListDeleteByPos(SeqList *psl, int pos);
+int SeqListDeleteByVal(SeqList *psl, DataType data);
+int SeqListFindByPos(SeqList *psl, int pos, DataType *ret);
+int SeqListFindByVal(SeqList *psl, DataType data);
+void SeqListSort(SeqList *psl);
+void SeqListReverse(SeqList *psl);
+int SeqListLength(SeqList *psl);
+int SeqListModifyByPos(SeqList *psl, int pos, DataType data);
+int SeqListModifyByVal(SeqList *psl, DataType dest, DataType data);
 
 void SeqListInit(SeqList *psl, int sz)
 {
-    assert(psl);
+    assert(psl != NULL);
     psl->capacity = sz >SEQLIST_DEFAULT_SIZE ? sz :SEQLIST_DEFAULT_SIZE;
     psl->base = (DataType*)malloc(sizeof(DataType)*(psl->capacity));
     psl->size = 0;
@@ -34,7 +44,7 @@ void SeqListInit(SeqList *psl, int sz)
 void SeqListShow(SeqList *psl)
 {
 	int i = 0;
-    assert(psl);
+    assert(psl != NULL);
     if(0 == psl->size)
     {
         printf("顺序表为空!\n");
@@ -61,13 +71,14 @@ int SeqListPushBack(SeqList *psl, DataType data)
 
 int SeqListIsFull(SeqList *psl)
 {
+    assert(psl != NULL);
     return (psl->capacity) <= (psl->size);
 }
 
 int SeqListPushFront(SeqList *psl, DataType data)
 {
 	int i = 0;
-    assert(psl);
+    assert(psl != NULL);
     if(SeqListIsFull(psl))
     {
         printf("顺序表已满,%d不能插入!\n", data);
@@ -84,7 +95,7 @@ int SeqListPushFront(SeqList *psl, DataType data)
 
 int SeqListPopBack(SeqList *psl)
 {
-    assert(psl);
+    assert(psl != NULL);
     if(0 == psl->size)
     {
         printf("顺序表为空!\n");
@@ -97,7 +108,7 @@ int SeqListPopBack(SeqList *psl)
 int SeqListPopFront(SeqList *psl)
 {
 	int i = 0;
-    assert(psl);
+    assert(psl != NULL);
     if(0 == psl->size)
     {
         printf("顺序表为空!\n");
@@ -114,7 +125,7 @@ int SeqListPopFront(SeqList *psl)
 int SeqListInsertByPos(SeqList *psl, int pos, DataType data)
 {
 	int i = 0;
-    assert(psl);
+    assert(psl != NULL);
     if(SeqListIsFull(psl))
     {
         printf("顺序表已满,%d不能插入!\n", data);
@@ -137,6 +148,7 @@ int SeqListInsertByPos(SeqList *psl, int pos, DataType data)
 int SeqListInsertByVal(SeqList *psl, DataType data)
 {
 	int i = 0;
+    assert(psl != NULL);
     if(SeqListIsFull(psl))
     {
         printf("顺序表已满,%d不能插入!\n", data);
@@ -154,9 +166,10 @@ int SeqListInsertByVal(SeqList *psl, DataType data)
 	return 1;
 }
 
-int DeleteByPos(SeqList *psl, int pos)
+int SeqListDeleteByPos(SeqList *psl, int pos)
 {
 	int i = 0;
+    assert(psl != NULL);
     if(0 == psl->size)
     {
         printf("顺序表为空!\n");
@@ -170,6 +183,128 @@ int DeleteByPos(SeqList *psl, int pos)
 	for(i = pos; i < psl->size-1; ++i)
 		psl->base[i] = psl->base[i+1];
 	--(psl->size);
+	return 1;
+}
+
+int SeqListDeleteByVal(SeqList *psl, DataType data)
+{
+	int ret = SeqListFindByVal(psl, data);//返回要删除数据的位置
+    assert(psl != NULL);
+    if(0 == psl->size)
+        return 0;
+	if(-1 == ret)
+	{
+		printf("要删除的数据不存在!\n");
+		return 0;
+	}
+	return SeqListDeleteByPos(psl, ret);
+}
+
+int SeqListFindByPos(SeqList *psl, int pos, DataType *ret)
+{
+	assert(psl != NULL);
+    if(0 == psl->size)
+    {
+        printf("顺序表为空!\n");
+        return 0;
+    }
+    if(pos < 0 || pos > psl->size-1)
+    {
+        printf("输入的位置非法!\n");
+        return 0;
+    }
+	*ret = psl->base[pos];
+	return 1;
+}
+
+int SeqListFindByVal(SeqList *psl, DataType data)
+{
+	int i = 0;
+	assert(psl != NULL);
+    if(0 == psl->size)
+    {
+        printf("顺序表为空!\n");
+        return -1;
+    }
+	for(i = 0; i < psl->size; ++i)
+	{
+		if(psl->base[i] == data)
+			return i;
+	}
+	return -1;
+}
+
+void Swap(DataType *pa, DataType *pb)
+{
+	DataType swap;
+	swap = *pa;
+	*pa = *pb;
+	*pb = swap;
+}
+
+void SeqListSort(SeqList *psl)
+{
+	int i = 0, j = 0;
+    assert(psl != NULL);
+	for(i = 0; i < psl->size-1; ++i)
+	{
+		for(j = 0; j < psl->size-i-1; ++j)
+		{
+			if(psl->base[j] > psl->base[j+1])
+			{
+				Swap(&(psl->base[j]), &(psl->base[j+1]));
+			}
+		}
+	}
+}
+
+void SeqListReverse(SeqList *psl)
+{
+	int left = 0;
+	int right = psl->size - 1;
+	assert(psl != NULL);
+	while(left < right)
+	{
+		Swap(&(psl->base[left]), &(psl->base[right]));
+		++left;
+		--right;
+	}
+}
+
+int SeqListLength(SeqList *psl)
+{
+	assert(psl != NULL);
+	return psl->size;
+}
+
+void SeqListClear(SeqList *psl)
+{
+	assert(psl != NULL);
+	psl->size = 0;
+}
+
+int SeqListModifyByPos(SeqList *psl, int pos, DataType data)
+{
+	assert(psl != NULL);
+    if(pos < 0 || pos > psl->size-1)
+    {
+        printf("输入的位置非法!\n");
+        return 0;
+    }
+	psl->base[pos] = data;
+	return 1;
+}
+
+int SeqListModifyByVal(SeqList *psl, DataType dest, DataType data)
+{
+	int ret = SeqListFindByVal(psl, dest);
+	assert(psl != NULL);
+	if(-1 == ret)
+	{
+		printf("数据%d不存在!\n", dest);
+		return 0;
+	}
+	psl->base[ret] = data;
 	return 1;
 }
 #endif
