a16c97bab50afef9f7a9976c33e938d6d0c840b4
haohao <1783692558@qq.com>
2019-11-06 13:09:52 +0800

    提交数据结构（排序）代码

--- DataStruct/sort.c
+++ DataStruct/sort.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-	int ar[] = { 9, 1, 2, 5, 7, 4, 8, 6, 3, 5};
+	//int ar[] = { 9, 1, 2, 5, 7, 4, 8, 6, 3, 5 };
+	int ar[] = { 0, 9, 1, 2, 5, 7, 4, 8, 6, 3, 5 };//”√”⁄InsertSort_2
 	int n = sizeof(ar) / sizeof(ar[0]);
 	PrintArray(ar, 0, n-1);
-	TestSort(ar, 0, n-1);
+	//TestSort(ar, 0, n-1);
+	TestSort(ar, 1, n-1);
 	TestSortEfficiency();
 	return 0;
 }
--- DataStruct/sort.h
+++ DataStruct/sort.h
@@ -60,29 +60,53 @@ void InsertSort_2(int *ar, int left, int right)
 
 void BinInsertSort(int *ar, int left, int right)//二分插入排序
 {
-	
+	int i = 0;
+	for(i = left+1; i <= right; ++i)
+	{
+		int j = 0;
+		int low = left;
+		int high = i - 1;
+		int mid = 0;
+		int tmp = ar[i]; 
+		while(low <= high)
+		{
+			mid = (low + high) / 2;
+			if(ar[i] >= ar[mid])
+				low = mid + 1;
+			if(ar[i] < ar[mid])
+				high = mid - 1;
+		}
+		for(j = i; j > low; --j)
+		{
+			ar[j] = ar[j-1];
+		}
+		ar[low] = tmp;
+	}
 }
 
 void TestSort(int *ar, int left, int right)
 {
 	//InsertSort_1(ar, left, right);//插入排序
 	InsertSort_2(ar, left, right);
+	//BinInsertSort(ar, left, right);
 	PrintArray(ar, left, right);
 }
 
 void TestSortEfficiency()
 {
-	int n = 100000;
+	int n = 10000;
 	int i = 0;
 	time_t start = 0;
 	time_t end = 0;
 	int *a1 = (int*)malloc(sizeof(int) * n);
 	int *a2 = (int*)malloc(sizeof(int) * n);
+	int *a3 = (int*)malloc(sizeof(int) * n);
 	srand(time(0));
 	for(i = 0; i < n; ++i)
 	{
 		a1[i] = rand();
 		a2[i] = a1[i];
+		a3[i] = a1[i];
 	}
 	start = clock();
 	InsertSort_1(a1, 0, n-1);
@@ -92,5 +116,9 @@ void TestSortEfficiency()
 	InsertSort_2(a2, 0, n-1);
 	end = clock();
 	printf("InsertSort_2: %u\n", end - start);
+	start = clock();
+	BinInsertSort(a3, 0, n-1);
+	end = clock();
+	printf("BinInsertSort: %u\n", end - start);
 }
 #endif
