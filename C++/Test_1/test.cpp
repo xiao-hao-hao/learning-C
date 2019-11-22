1dc7978bc6d7276faa9315e9bebcf506468f25b9
haohao <1783692558@qq.com>
2019-11-22 14:09:04 +0800

    提交代码（C++: Test_1）

--- C++/Test_1/test.cpp
+++ C++/Test_1/test.cpp
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 
 using namespace std;
@@ -35,7 +34,37 @@ void TestFunc(int a)
 	cout<<a<<endl;
 }
 
+int Add(int left, int right)
+{
+	return left + right;
+}
 
+double Add(double left, double right)
+{
+	return left + right;
+}
+
+long Add(long left, long right)
+{
+	return left + right;
+}
+
+//下面两个函数无法形成重载
+void TestFunc1(int a = 10)
+{
+	cout<<"void TestFunc(int)"<<endl;
+}
+
+//void TestFunc1(int a)
+//{
+//	cout<<"void TestFunc(int)"<<endl;
+//}
+
+extern "C" int ADD(int left, int right)
+{
+	return 0;
+}
+//int ADD(int left, int right);
 //using myspace::fun;//使用命名空间的成员方法1
 using namespace myspace;//使用命名空间的成员方法2
 int main()
@@ -44,13 +73,19 @@ int main()
 	int a = 10;
 	char ch = 'A';
 	double b= 12.34;
-	cin>>a;
-	cout<<a<<endl;//printf("%d\n", a);
-	cout<<ch<<endl;//printf("%c\n", ch);
-	cout<<b<<endl;//printf("%f\n", d);
+	//cin>>a;
+	//cout<<a<<endl;//printf("%d\n", a);
+	//cout<<ch<<endl;//printf("%c\n", ch);
+	//cout<<b<<endl;//printf("%f\n", d);
 	//myspace::fun();//使用命名空间的成员方法3
 	fun();
 	yourspace::fun();
 	TestFunc();
 	TestFunc(10);
+	Add(10, 20);
+	Add(10.0, 20.0);
+	Add(10L, 20L);
+	ADD(1, 2);
+	TestFunc1();
+	return 0;
 }
