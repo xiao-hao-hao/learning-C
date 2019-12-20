cf23702065d4feeaab1de3c6f5b26f2d21791505
haohao <1783692558@qq.com>
2019-12-20 10:23:51 +0800

    提交C++代码(Date:增加了日期减日期求天数和根据日期求星期几)

--- C++/Date/Date/date.cpp
+++ C++/Date/Date/date.cpp
@@ -21,6 +21,8 @@ public:
 	bool operator<=(const Date& d)const;
 	bool operator==(const Date& d)const;
 	bool operator!=(const Date& d)const;
+public:
+	int GetWeek();
 private:
 	int _year;
 	int _month;
@@ -64,6 +66,10 @@ bool Date::operator!=(const Date &d)const
 
 bool Date::operator>(const Date &d)const
 {
+	return (_year > d._year) || 
+		(_year == d._year && _year > d._year) || 
+		(_year == d._year && _year == d._year && _month > d._month);
+	/*
 	if (_year != d._year)
 		return _year > d._year;
 	if (_month != d._month)
@@ -71,6 +77,7 @@ bool Date::operator>(const Date &d)const
 	if (_day != d._day)
 		return _day > d._day;
 	return false;
+	*/
 }
 
 bool Date::operator<=(const Date &d)const
@@ -80,6 +87,10 @@ bool Date::operator<=(const Date &d)const
 
 bool Date::operator<(const Date &d)const
 {
+	return (_year < d._year) || 
+		(_year == d._year && _month < d._month) || 
+		(_year == d._year && _month == d._month && _day < d._day);//两个日期相比，只有这三种可能会使左边日期小于右边日期
+	/*
 	if (_year != d._year)
 		return _year < d._year;
 	if (_month != d._month)
@@ -87,6 +98,7 @@ bool Date::operator<(const Date &d)const
 	if (_day != d._day)
 		return _day < d._day;
 	return false;
+	*/
 }
 
 bool Date::operator>=(const Date &d)const
@@ -109,13 +121,20 @@ bool IsLeap(int year)
 
 int GetDayByYearMonth(int year, int month)
 {
-	int day[13] = { 29, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int day[13] = { 29, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	if (month == 2)
 	{
 		if (IsLeap(year))
 			month = 0;
 	}
 	return day[month];
+}
+
+int GetDayByYear(int year)
+{
+	if (IsLeap(year))
+		return 366;
+	return 365;
 }
 
 Date Date::operator+(int days)
@@ -131,41 +150,85 @@ Date Date::operator+(int days)
 			++tmp._year;
 		}
 		days -= mdays;
-		mdays = GetDayByYearMonth(tmp._day, tmp._month);
+		mdays = GetDayByYearMonth(tmp._year, tmp._month);
 	}
 	tmp._day += days;
 	return tmp;
 }
 
-/*
-Date Date::operator-(int day)
+
+Date Date::operator-(int days)
 {
+	int mdays;
 	Date tmp = *this;
-	if (day < tmp._day)
-		tmp._day -= day;
-
-}
-*/
-
-int Date::operator-(const Date& d)const
-{
-	Date tmp1, tmp2;
-	int count = 0;
-	if (*this < d)
+	while (tmp._day - days < 1)
 	{
-		tmp1 = *this;
-		tmp2 = d;
+		--(tmp._month);
+		if (tmp._month < 1)
+		{
+			tmp._month = 12;
+			--(tmp._year);
+		}
+		mdays = GetDayByYearMonth(tmp._year, tmp._month);
+		days -= mdays;
+	}
+	tmp._day -= days;
+	return tmp;
+}
+
+int GetLastYearDays(int year, int month, int day)//获得一个日期最后一个不完整的年的天数
+{
+	int days = 0;
+	for (int i = 1; i < month; ++i)
+	{
+		days += GetDayByYearMonth(year, i);
+	}
+	days += day;
+	return days;
+}
+
+void Swap(const Date &dt1, const Date &dt2, Date &tmp1, Date &tmp2)
+{
+	if (dt1 < dt2)
+	{
+		tmp1 = dt1;
+		tmp2 = dt2;
 	}
 	else
 	{
-		tmp1 = d;
-		tmp2 = *this;
+		tmp1 = dt2;
+		tmp2 = dt1;
 	}
+}
+
+int Date::operator-(const Date& d)const
+{
+
+	Date tmp1, tmp2;//tmp1存放小日期
+	int count = 0;//tmp2存放大日期
+	Swap(*this, d, tmp1, tmp2);//tmp1存放小日期，tmp2存放大日期
+	int last_year_days1 = GetLastYearDays(tmp1._year, tmp1._month, tmp1._day);
+	int last_year_days2 = GetLastYearDays(tmp2._year, tmp2._month, tmp2._day);
+	if (last_year_days2 >= last_year_days1)//将两个日期最后一年的天数相减，不够借位
+		count = last_year_days2 - last_year_days1;
+	else
+	{
+		count = GetDayByYear(tmp2._year - 1) - last_year_days1 + last_year_days2;
+		--(tmp2._year);
+	}
+
+	for (int i = tmp1._year; i < tmp2._year; ++i)//将剩下的年相差的天数算出来
+	{
+		count += GetDayByYear(i);
+	}
+
+	/*
 	while (tmp1 < tmp2)
 	{
 		++tmp1;
 		++count;
 	}
+	*/
 	return count;
 }
 
@@ -195,14 +258,35 @@ Date Date::operator--(int)//后置--
 	return tmp;
 }
 
+//W=(Y-1)+[(Y-1)/4]-[(Y-1)/100]+[(Y-1)/400]+D
+//return W % 7
+//D代表day在这一年的累计天数
+//int Date::GetWeek()
+//{
+//	int D = GetLastYearDays(_year, _month, _day);
+//	int W = (_year - 1) + (_year - 1) / 4 - (_year - 1) / 100 +
+//		(_year - 1) / 400 + D;
+//	return W % 7;
+//}
+
+int Date::GetWeek()
+{
+	int W = _day + 2 * _month + 3 * (_month + 1) / 5 + _year + _year / 4 
+		- _year/100 + _year / 400 + 1;
+	W = W % 7;
+	return W % 7;
+}
+
 int main()
 {
-	Date dt1(0000, 12, 9);
-	Date dt2(1258951, 12, 9);
+	Date dt1(2020, 3, 18);
+	Date dt2(5258, 6, 29);
+	//Date dt2(2019, 10, 30);
 	//++dt1;//dt1.operator++()
-	dt1++;//dt1.operator++(int)
-	cout << dt1 - dt2 << endl;
-	cout << dt1 << endl;
+	//dt1++;//dt1.operator++(int)
+	//cout << dt1 - 21654 << endl;
+	//cout << dt1 - dt2 << endl;
+	cout << dt1.GetWeek() << endl;
 	return 0;
 }
 
