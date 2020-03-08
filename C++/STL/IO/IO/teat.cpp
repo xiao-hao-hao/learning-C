b979cdd91bd4452db0c4963b93de714a80a4c741
haohao <1783692558@qq.com>
2020-03-08 22:35:09 +0800

    提交C++代码(IO)

--- C++/STL/IO/IO/teat.cpp
+++ C++/STL/IO/IO/teat.cpp
@@ -55,70 +55,121 @@ using namespace std;
 //	return 0;
 //}
 
-#include <fstream>
+//#include <fstream>
+//
+//// 使用文件IO流用文本及二进制方式演示读写配置文件
+//struct ServerInfo
+//{
+//	char _ip[32]; // ip
+//	int _port; // 端口
+//};
+//struct ConfigManager
+//{
+//public:
+//	ConfigManager(const char* configfile = "bitserver.config")
+//		:_configfile(configfile)
+//	{}
+//	void WriteBin(const ServerInfo& info)
+//	{
+//		// 这里注意使用二进制方式打开写
+//		ofstream ofs(_configfile, ios::out | ios::binary);
+//		ofs.write((const char*)&info, sizeof(ServerInfo));
+//		ofs.close();
+//	}
+//	void ReadBin(ServerInfo& info)
+//	{
+//		// 这里注意使用二进制方式打开读
+//		ifstream ifs(_configfile, ifstream::in | ifstream::binary);
+//		ifs.read((char*)&info, sizeof(ServerInfo));
+//		ifs.close();
+//	}
+//	void WriteText(const ServerInfo& info)
+//	{
+//		// 这里会发现IO流写整形比C语言那套就简单多了，
+//		// C 语言得先把整形itoa再写
+//		ofstream ofs(_configfile, ios::out);
+//		ofs << info._ip << endl;
+//		ofs << info._port << endl;
+//		ofs.close();
+//	}
+//	void ReadText(ServerInfo& info)
+//	{
+//		// 这里会发现IO流读整形比C语言那套就简单多了，
+//		// C 语言得先读字符串，再atoi
+//		ifstream ifs(_configfile, ios::in);
+//		ifs >> info._ip;
+//		ifs >> info._port;
+//		ifs.close();
+//	}
+//private:
+//	string _configfile; // 配置文件
+//};
+//int main()
+//{
+//	ConfigManager cfgMgr;
+//	ServerInfo wtinfo;
+//	ServerInfo rdinfo;
+//	wtinfo = { "127.0.0.1", 8080 };
+//	// //二进制读写
+//	cfgMgr.WriteBin(wtinfo);
+//	cfgMgr.ReadBin(rdinfo);
+//	cout << rdinfo._ip << endl;
+//	cout << rdinfo._port << endl;
+//	// 文本读写
+//	//cfgMgr.WriteText(wtinfo);
+//	//cfgMgr.ReadText(rdinfo);
+//	//cout << rdinfo._ip << endl;
+//	//cout << rdinfo._port << endl;
+//	return 0;
+//}
 
-// 使用文件IO流用文本及二进制方式演示读写配置文件
-struct ServerInfo
-{
-	char _ip[32]; // ip
-	int _port; // 端口
-};
-struct ConfigManager
-{
-public:
-	ConfigManager(const char* configfile = "bitserver.config")
-		:_configfile(configfile)
-	{}
-	//void WriteBin(const ServerInfo& info)
-	//{
-	//	// 这里注意使用二进制方式打开写
-	//	ofstream ofs(_configfile, ios::out | ios::binary);
-	//	ofs.write((const char*)&info, sizeof(ServerInfo));
-	//	ofs.close();
-	//}
-	//void ReadBin(ServerInfo& info)
-	//{
-	//	// 这里注意使用二进制方式打开读
-	//	ifstream ifs(_configfile, ifstream::out | ifstream::binary);
-	//	ifs.read((char*)&info, sizeof(ServerInfo));
-	//	ifs.close();
-	//}
-	void WriteText(const ServerInfo& info)
-	{
-		// 这里会发现IO流写整形比C语言那套就简单多了，
-		// C 语言得先把整形itoa再写
-		ofstream ofs(_configfile);
-		ofs << info._ip << endl;
-		ofs << info._port << endl;
-		ofs.close();
-	}
-	void ReadText(ServerInfo& info)
-	{
-		// 这里会发现IO流读整形比C语言那套就简单多了，
-		// C 语言得先读字符串，再atoi
-		ifstream ifs(_configfile);
-		ifs >> info._ip;
-		ifs >> info._port;
-		ifs.close();
-	}
-private:
-	string _configfile; // 配置文件
-};
+// extract to string
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//	std::string name;
+//	std::cout << "Please, enter your full name: ";
+//	cout << (bool)std::getline(std::cin, name);
+//	std::cout << "Hello, " << name << "!\n";
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char m[20];
+//	cout << "please: ";
+//	fflush(stdin);
+//	cin.getline(m, 21);
+//	cout << m << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//	string line;
+//	while (getline(cin, line))
+//		cout << line << endl;
+//	return 0;
+//}
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
 int main()
 {
-	ConfigManager cfgMgr;
-	ServerInfo wtinfo;
-	ServerInfo rdinfo;
-	wtinfo = { "127.0.0.1", 8080 };
-	// 二进制读写
-	//cfgMgr.WriteBin(wtinfo);
-	//cfgMgr.ReadBin(rdinfo);
-	cout << rdinfo._ip << endl;
-	cout << rdinfo._port << endl;
-	// 文本读写
-	cfgMgr.WriteText(wtinfo);
-	cfgMgr.ReadText(rdinfo);
-	cout << rdinfo._ip << endl;
-	cout << rdinfo._port << endl;
+	string line;
+	char a[20];
+	cout << (bool)cin.getline(a, 5);
+	//printf("%d", (bool)cin.getline(a, 5));
 	return 0;
 }
