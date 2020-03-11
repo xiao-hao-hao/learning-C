93bf150cbc5d164f7c3babfb471a4b1ed0b491e9
haohao <1783692558@qq.com>
2020-03-11 23:01:01 +0800

    提交C++代码(继承)

---
+++ C++/STL/Inheritance_2/Inheritance_2/test.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+//class Person
+//{
+//public:
+//	Person() : _name(1)
+//	{ ++_count; }
+//protected:
+//	int _name;
+//	//string _name; // 姓名
+//public:
+//	static int _count; // 统计人的个数。
+//};
+//int Person::_count = 0;
+//class Student : public Person
+//{
+//public:
+//	Student() : _stuNum(2)
+//	{
+//		cout << _count << endl;
+//	}
+//protected:
+//	int _stuNum; // 学号
+//};
+//class Graduate : public Student
+//{
+//protected:
+//	string _seminarCourse; // 研究科目
+//};
+//void main()
+//{
+//	Student s1;
+//	Student s2;
+//	Student s3;
+//	Graduate s4;
+//	cout << " 人数 :" << Person::_count << endl;
+//	Student::_count = 0;
+//	cout << " 人数 :" << Person::_count << endl;
+//}
+
+//class Student;
+//class Person
+//{
+//public:
+//	friend void Display(const Person& p, const Student& s);
+//protected:
+//	string _name; // 姓名
+//};
+//class Student : public Person
+//{
+//	//friend void Display(const Person& p, const Student& s);
+//protected:
+//	int _stuNum; // 学号
+//};
+//void Display(const Person& p, const Student& s)
+//{
+//	cout << p._name << endl;
+//	cout << s._stuNum << endl;
+//}
+//void main()
+//{
+//	Person p;
+//	Student s;
+//	Display(p, s);
+//}
