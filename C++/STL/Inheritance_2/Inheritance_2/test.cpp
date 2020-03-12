54676bee5428d699db405c6e09f63a3715e0b904
haohao <1783692558@qq.com>
2020-03-12 19:57:42 +0800

    提交C++代码(继承)

--- C++/STL/Inheritance_2/Inheritance_2/test.cpp
+++ C++/STL/Inheritance_2/Inheritance_2/test.cpp
@@ -2,7 +2,95 @@
 #include <string>
 using namespace std;
 
+class A {
+public:
+	int _a;
+};
+ //class B : public A
+class B : virtual public A
+{
+public:
+	int _b;
+};
+class C : public A
+//class C : virtual public A
+{
+public:
+	int _c;
+};
+class D : virtual public B, virtual public C {
+public:
+	int _d;
+};
+int main()
+{
+	D d;
+	B b;
+	d.B::_a = 1;
+	d.C::_a = 2;
+	d._b = 3;
+	d._c = 4;
+	d._d = 5;
+	return 0;
+}
 
+//class Person
+//{
+//public:
+//	string _name; // 姓名
+//};
+//class Student : virtual public Person
+//{
+//protected:
+//	int _num; //学号
+//};
+//class Teacher : virtual public Person
+//{
+//protected:
+//	int _id; // 职工编号
+//};
+//class Assistant : public Student, public Teacher
+//{
+//protected:
+//	string _majorCourse; // 主修课程
+//};
+//void main()
+//{
+//	Assistant a;
+//	a._name = "peter";
+//}
+
+//class Person
+//{
+//public:
+//	string _name; // 姓名
+//};
+//
+//class Student : public Person
+//{
+//protected:
+//	int _num; //学号
+//};
+//class Teacher : public Person
+//{
+//protected:
+//	int _id; // 职工编号
+//};
+//class Assistant : public Student, public Teacher
+//{
+//protected:
+//	string _majorCourse; // 主修课程
+//};
+//void main()
+//{
+//	// 这样会有二义性无法明确知道访问的是哪一个
+//	Assistant a;
+//	//a._name = "peter";
+//
+//	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
+//	a.Student::_name = "xxx";
+//	a.Teacher::_name = "yyy";
+//}
 
 //class Person
 //{
