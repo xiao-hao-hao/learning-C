64baebbfe6f7d25a74c6e224f3776656f13283ac
haohao <1783692558@qq.com>
2020-04-27 16:09:52 +0800

    提交C++代码(二叉搜索树和avl数)

--- C++/STL/bst_avl/bst_avl/test.cpp
+++ C++/STL/bst_avl/bst_avl/test.cpp
@@ -46,9 +46,9 @@ protected:
 		if (t == nullptr)
 			return nullptr;
 		if (key < t->data)
-			Search(t->leftChild, key);
+			return Search(t->leftChild, key);
 		else if (key > t->data)
-			Search(t->rightChild, key);
+			return Search(t->rightChild, key);
 		else
 			return t;
 	}
@@ -60,9 +60,9 @@ protected:
 			return true;
 		}
 		else if (x < t->data)
-			Insert(t->leftChild, x);
+			return Insert(t->leftChild, x);
 		else if (x > t->data)
-			Insert(t->rightChild, x);
+			return Insert(t->rightChild, x);
 		else
 			return false;
 	}
@@ -70,11 +70,37 @@ protected:
 	{
 		if (t == nullptr)
 			return false;
-
+		if (key < t->data)
+			return Remove(t->leftChild, key);
+		else if (key > t->data)
+			return Remove(t->rightChild, key);
+		else
+		{
+			if (t->leftChild!=nullptr && t->rightChild!=nullptr)
+			{
+				BSTNode<Type> *p = t->leftChild;
+				while (p->rightChild != nullptr)
+					p = p->rightChild;
+				t->data = p->data;
+				Remove(t->leftChild, p->data);
+			}
+			else
+			{
+				BSTNode<Type> *tmp = t;
+				if (t->leftChild != nullptr)
+					t = t->leftChild;
+				else
+					t = t->rightChild;
+				delete tmp;
+				return true;
+			}
+		}
 	}
 private:
 	BSTNode<Type> *root;
 };
+
+
 
 int main()
 {
@@ -83,5 +109,44 @@ int main()
 	for (const auto &e : ar)
 		bst.Insert(e);
 	BSTNode<int>* pb = bst.Search(7);
+	bst.Remove(2);
 	return 0;
 }
+
+//bool Remove(BSTNode<Type> *&t, const Type &key)
+//{
+//	if (t == nullptr)
+//		return false;
+//	if (key < t->data)
+//		return Remove(t->leftChild, key);
+//	else if (key > t->data)
+//		return Remove(t->rightChild, key);
+//	else
+//	{
+//		if (t->leftChild == nullptr && t->rightChild == nullptr)
+//		{
+//			delete t;
+//			t = nullptr;
+//		}
+//		else if (t->leftChild != nullptr && t->rightChild == nullptr)
+//		{
+//			BSTNode<Type> *tmp = t;
+//			t = t->leftChild;//t是leftChild或者rightChild的引用
+//			delete tmp;
+//		}
+//		else if (t->leftChild == nullptr && t->rightChild != nullptr)
+//		{
+//			BSTNode<Type> *tmp = t;
+//			t = t->rightChild;
+//			delete tmp;
+//		}
+//		else//当要删除的节点的左右子节点都不为空时，找到该节点左子树的最大值
+//		{
+//			BSTNode<Type> *p = t->leftChild;
+//			while (p->rightChild != nullptr)
+//				p = p->rightChild;
+//			t->data = p->data;//然后把这个最大值给了要删除的节点
+//			Remove(t->leftChild, p->data);//然后在左树中把这个最大值给删掉
+//		}
+//	}
+//}
